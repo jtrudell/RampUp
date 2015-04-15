4e43cf192233f4e7ac069e9c404c2be3009b8c52
Jeanette Trudell <jtrudell@gmail.com>
2015-04-14 21:17:07 -0500

    revise

--- adventure.rb
+++ adventure.rb
@@ -1,14 +1,4 @@
-
-class Path
-#  @name
-#  @num_of_weapons
-#  @health
-#  @direction
-
-#def num_of_enemies
-
-#  end
-end
+# In-class exercise April 14, 2015.
 
 class Hero
 	attr_accessor :name, :health, :strength
@@ -39,12 +29,24 @@ class Weapon
 	end
 end
 
-def power #(between weapons and heros, weapon and enemies)
+class Path
+	attr_accessor :name, :direction,:num_of_weapons, :health_points
 
+	def initialize(name, direction, num_of_weapons, health_points)
+		@name = name
+		@direction = direction
+		@num_of_weapons = num_of_weapons
+		@health_points = health_points
+	end
+end
+
+def power (character, weapon)
+	power = character.strength * weapon.damage
+	puts "#{character.name} has #{power} power using #{weapon.name}."
 end
 
 def battle (hero, enemy)
-puts "#{hero.name} and #{enemy.name} engage in battle!"
+  puts "#{hero.name} and #{enemy.name} engage in battle!"
 	if hero.strength > enemy.strength
 		enemy.strength -= 5
 		puts "#{hero.name} wins the battle! #{enemy.name}'s strength is reduced to #{enemy.strength}." 
@@ -54,11 +56,20 @@ puts "#{hero.name} and #{enemy.name} engage in battle!"
 	end
 end
 
+def choose_path(path, character)
+	puts "#{character.name} is walking #{path.direction} on #{path.name}."
+end
 
+our_hero = Hero.new("Our Hero", 100, 100)
+bad_guy = Enemy.new("Bad Guy", 100, 50)
+axe = Weapon.new("Axe", 5)
+castle_lane = Path.new("Castle Lane", "North", 5, 20)
 
-blurghero = Hero.new("blurghero", 100, 100)
-badguy = Enemy.new("badguy", 100, 100)
-axe = Weapon.new("axe", 5)
+battle(our_hero, bad_guy)
 
-battle(blurghero, badguy)
+power(our_hero, axe)
+
+power(bad_guy, axe)
+
+choose_path(castle_lane, our_hero)
 
