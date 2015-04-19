156359cce63b4c88d20d691c7b49d10a1ca7addc
Jeanette Trudell <jtrudell@gmail.com>
2015-04-18 21:36:00 -0500

    works and done

--- oldroman.rb
+++ oldroman.rb
@@ -1,7 +1,7 @@
 # Exercise from Section 9.5 of Chris Pine's "Learn to Program" Ruby book. 
 # Convert integers to "old" type Roman numberals (e.g., Roman numerals that didn't 
 # use subtraction: 9 is VIIII, not IX).
-# Jen Trudell March 21, 2015
+# Jen Trudell April 18, 2015
 
 
 def convert_roman(number)
@@ -12,54 +12,61 @@ roman_10 = "X"
 roman_50 = "L"
 roman_100 = "C"
 
-if number < 5
-	number.times do 
-		print roman_1
+	if number < 5
+		number.times do 
+			print roman_1
+		end
+		puts
+	elsif number >= 5 && number < 10
+		print roman_5
+		remainder = number % 5
+		remainder.times do
+			print roman_1
+		end
+		puts
+	elsif number >=10 && number < 50
+		divide_10 = number / 10
+		divide_10.times do 
+			print roman_10
+		end
+		remainder_10 = number % 10
+		deal_with_v(remainder_10)
+		puts
+	elsif number >=50 && number < 100
+		print roman_50
+		divide_10 = (number - 50) / 10
+		divide_10.times do 
+			print roman_10
+		end
+		remainder_10 = number % 10
+		deal_with_v(remainder_10)
+		puts
+	elsif number == 100
+		puts roman_100
+	else
+	puts "Does not compute!"
 	end
-	puts
-elsif number >= 5 && number < 10
-	print roman_5
-	remainder = number % 5
-	remainder.times do
-		print roman_1
-	end
-	puts
-
-#ADD V FOR NUMBERS GREATER THAN 10 (e.g. XV rather than XIIIII)
-elsif number >=10 && number < 50
-	divide_10 = number / 10
-	divide_10.times do 
-		print roman_10
-	end
-	remainder_10 = number % 10
-	remainder_10.times do
-		print roman_1
-	end
-	puts
-
-elsif number >=50 && number < 100
-	print roman_50
-	divide_10 = (number - 50) / 10
-	divide_10.times do 
-		print roman_10
-	end
-	remainder_10 = number % 10
-	remainder_10.times do
-		print roman_1
-	end
-	puts
-
-elsif number == 100
-	puts roman_100
-else
-	puts "hi"
 end
+
+def deal_with_v(remainder_10)
+	if remainder_10 >= 5
+		print "V"
+		remainder_10 -= 5
+		remainder_10.times do
+			print "I"
+		end
+	else	
+		remainder_10.times do
+			print "I"
+		end
+	end
 end
+
 	
 puts "Enter a number between 1 and 100."
 user_number = gets.chomp
 number = user_number.to_i
-if number < 0 || number > 100
+if number <= 0 || number > 100
 	puts "Try again."
 else 
 	puts "This program will convert #{user_number} to an old school Roman numeral."
