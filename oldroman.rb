76c76094c88cf448856798c7c5f7375429ad7e17
Jeanette Trudell <jtrudell@gmail.com>
2015-04-21 22:30:10 -0500

    clean up

--- oldroman.rb
+++ oldroman.rb
@@ -1,7 +1,7 @@
 # Exercise from Section 9.5 of Chris Pine's "Learn to Program" Ruby book. 
-# Convert integers to "old" type Roman numberals (e.g., Roman numerals that didn't 
+# Convert integers to "old" type Roman numerals (e.g., Roman numerals that didn't 
 # use subtraction: 9 is VIIII, not IX).
-# Jen Trudell April 18, 2015
+# Jen Trudell April 21, 2015
 
 
 def convert_roman(number)
@@ -11,6 +11,8 @@ roman_5 = "V"
 roman_10 = "X"
 roman_50 = "L"
 roman_100 = "C"
+
+print "#{number} converted into an old school Roman numeral is: "
 
 	if number < 5
 		number.times do 
@@ -30,7 +32,7 @@ roman_100 = "C"
 			print roman_10
 		end
 		remainder_10 = number % 10
-		deal_with_v(remainder_10)
+		include_v(remainder_10)
 		puts
 	elsif number >=50 && number < 100
 		print roman_50
@@ -39,7 +41,7 @@ roman_100 = "C"
 			print roman_10
 		end
 		remainder_10 = number % 10
-		deal_with_v(remainder_10)
+		include_v(remainder_10)
 		puts
 	elsif number == 100
 		puts roman_100
@@ -48,7 +50,7 @@ roman_100 = "C"
 	end
 end
 
-def deal_with_v(remainder_10)
+def include_v(remainder_10)
 	if remainder_10 >= 5
 		print "V"
 		remainder_10 -= 5
@@ -62,13 +64,30 @@ def deal_with_v(remainder_10)
 	end
 end
 
-	
-puts "Enter a number between 1 and 100."
-user_number = gets.chomp
-number = user_number.to_i
-if number <= 0 || number > 100
-	puts "Try again."
-else 
-	puts "This program will convert #{user_number} to an old school Roman numeral."
-	convert_roman(number)
+def prompt
+	print ">> "
 end
+
+def input_number
+	puts "Enter a number between 1 and 100."
+	prompt
+	user_number = gets.chomp
+	number = user_number.to_i
+	if number <= 0 || number > 100
+		puts "Try again. Type 'quit' to quit the program or hit enter to enter a number to convert."
+		prompt
+		answer=gets.chomp.downcase
+		if answer == "quit"
+			exit
+		else
+			input_number
+		end
+	else 
+		convert_roman(number)
+	end
+end
+puts
+puts "This program will convert a number you enter into an old school Roman numeral."
+puts "An 'old school' Roman numeral does not use subtraction (i.e., 9 is VIIII and not IX)."
+input_number
+
