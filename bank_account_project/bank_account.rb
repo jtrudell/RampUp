c7a29ca7f9466e09fc8b5f724c8850556085db56
Jen Trudell <jtrudell@gmail.com>
2015-05-02 21:27:37 -0500

    add yaml and write to customers.txt

--- bank_account_project/bank_account.rb
+++ bank_account_project/bank_account.rb
@@ -1,8 +1,9 @@
+#create Module customer, which includes class Person. Person includes name and address.
 module Customer
 
 	class Person
 
-		attr_accessor :name
+		attr_accessor :name, :address
 
 		def initialize(name, address)
 			@name = name
@@ -13,6 +14,7 @@ module Customer
 
 end
 
+#create class BankAccount, which includes Customer module. BankAccount includes account name, number and balance.
 class BankAccount
 
 include Customer
@@ -25,23 +27,27 @@ attr_accessor :name, :account_number, :balance
 		@balance = balance
 	end
 
+#account_balance method provides current account balance
 	def account_balance(deposit, withdrawal)
 		adjustment_to_balance = deposit - withdrawal
 		@balance = @balance + adjustment_to_balance
 	end
-		
+
+#deposit method handles deposits to accounts
 	def deposit
 		puts "How much would you like to deposit?"
 		amount_deposited = gets.chomp.to_i
 		self.account_balance(amount_deposited, withdrawal=0)
 	end
 
+#withdrawal method handles withdrawls from accounts
 	def withdrawal
 		puts "How much would you like to withdraw?"
 		amount_withdrawn = gets.chomp.to_i
 		self.account_balance(deposit = 0, amount_withdrawn)
 	end
 
+#status method prints out account name, account number, and balance
 	def status
 		puts "----Account Status----"
 		puts "Account Name: " + @name
@@ -51,20 +57,35 @@ attr_accessor :name, :account_number, :balance
 	end
 end
 
+#method to open a new account and write it to customers.txt
 def open_account
 include Customer
+require 'yaml'
 	puts "What is the customer's name?"
 	customer_name = gets.chomp
 	puts "What is the customer's address?"
 	customer_address = gets.chomp
-	Person.new(customer_name, customer_address)
+	new_customer = Person.new(customer_name, customer_address)
+	#write the new_customer object to the customers.txt file, converted to YAML format
+	File.open('customers.txt', 'a+') do |f|  
+  		f.write(new_customer.to_yaml)
+	end  
 end
 
-new_customer = open_account
 
-puts new_customer.name
+#method to bring up an account from customers.txt
+def load_account
+include Customer
+require 'yaml'
+	puts "What customer's account would you like to load?"
+	customer_name = gets.chomp
+	File.open('customers.txt', 'a+') do |f|
+	puts f.gets YAML::load(customer_name)
+	end
+end
 
-
+open_account
+load_account
 #my_account = BankAccount.new("Jen's Bank Account", 3334, 500)
 
 #my_account.status
