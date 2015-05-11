d6442a7482ba2873dfb968c7a6d3f96e41178acc
Jen Trudell <jtrudell@gmail.com>
2015-05-11 12:55:34 -0500

    added method for writing to file, fixed check_password method

--- bank_account_project/bank_account.rb
+++ bank_account_project/bank_account.rb
@@ -1,3 +1,5 @@
+# Final Project for Rampup Ruby class @ Startup Institute 
+# Jen Trudell May 11, 2015
 
 require './bank_module'
 require 'yaml'
@@ -6,45 +8,48 @@ def prompt
 	print ">> "
 end
 
-# method to open a new account and write it to customer's yaml file
+# method to open a new account and write BankAccount object to customer's yaml file
 def open_account
 include Bank
-	puts "What is the customer's name?"
+	puts "Welcome to the First Bank of Ruby! We are glad you have decided to bank with us."
+	puts "Please enter your first name: "
 	prompt
 	customer_name = gets.chomp
-	puts "What is the new account number?"
-	prompt
-	account_number = gets.chomp.to_i
-	puts "What is the opening balance of the account?"
+	puts "Enter the opening balance for your new account: "
 	prompt
 	opening_balance = gets.chomp.to_f
-	puts "Enter a password for the customer."
+	puts "Choose a password for your account: "
 	prompt
 	customer_password = gets.chomp
+	account_number = rand(1..1000)
+	puts "Congratulations! You have opened an account at First Bank of Ruby."
+	puts "Your account number is #{account_number}."
 	new_customer = BankAccount.new(customer_name, account_number, opening_balance, customer_password)
 	# write the new_customer object to the customer's yaml file
-	customer_file = "#{customer_name}" + ".yaml"
-	File.open(customer_file, 'w') do |file|  
-  		file.puts YAML::dump(new_customer)
-	end  
+	new_customer.update_record(customer_name)
+
 end
 
-
-
-# method to load account from customer's yaml file
+# method to load BankAccount object from customer's yaml file
 def load_account
 include Bank
-	puts "What customer's account would you like to load?"
+	puts "Please enter your first name."
 	prompt
 	customer_name = gets.chomp
 	customer_file = "#{customer_name}" + ".yaml"
-	File.open(customer_file, 'r') do |file|
-  	YAML::load(file.read)
+	# if file with the customer's name exists, loads the BankAccount object from the file
+	if File.exists?(customer_file)
+		File.open(customer_file, 'r') do |file|
+	  	YAML::load(file.read)
+	  end
+	# if customer doesn't exist, calls open_account to create a file/account for the customer, and then calls load_account
+	else
+		puts "It doesn't look like you have an account at First Bank of Ruby."
+		puts "Let's open a new account for you."
+		open_account
+		load_account
 	end 
 end
 
-
-#open_account
-test_account = load_account
-test_account.transactions
-
+# calls welcome method on BankAccount object that was loaded
+load_account.welcome
--- bank_account_project/bank_module.rb
+++ bank_account_project/bank_module.rb
@@ -1,9 +1,10 @@
+#create Bank module so BankAccount class can be pulled into bank_account.rb (for opening new accounts, transactions)
 module Bank
 require 'yaml'
 require 'formatador'
+
 # create class BankAccount. BankAccount object initilized with customer name, account number, balance and password.
 class BankAccount
-
 
 attr_accessor :name, :account_number, :balance, :password
 
@@ -24,6 +25,14 @@ attr_accessor :name, :account_number, :balance, :password
 		time.strftime("%m/%d/%Y at %I:%M%p")
 	end
 
+# update_record method updates (or creates, if it doesn't exist) customer's yaml file with BankAccount object of customer
+	def update_record(name)
+		customer_file = "#{name}" + ".yaml"
+				File.open(customer_file, 'w') do |file|  
+  			file.puts YAML::dump(self)
+  			end
+  	end
+  			
 # account_balance method provides current account balance
 	def account_balance(deposit, withdrawal)
 		adjustment_to_balance = deposit - withdrawal
@@ -36,7 +45,7 @@ attr_accessor :name, :account_number, :balance, :password
 		prompt
 		amount_deposited = gets.chomp.to_f
 		self.account_balance(amount_deposited, withdrawal=0)
-		puts Formatador.display_line("[green]You deposited $#{amount_deposited.to_s} on #{transaction_time}[/]\n") 
+		puts Formatador.display_line("[blue]You deposited $#{amount_deposited.to_s} on #{transaction_time}[/]\n") 
 	end
 
 # withdrawal method handles withdrawls from accounts
@@ -45,7 +54,7 @@ attr_accessor :name, :account_number, :balance, :password
 		prompt
 		amount_withdrawn = gets.chomp.to_f
 		self.account_balance(deposit = 0, amount_withdrawn)
-		puts Formatador.display_line("[green]You withdrew $#{amount_withdrawn.to_s} on #{transaction_time}[/]\n") 
+		puts Formatador.display_line("[blue]You withdrew $#{amount_withdrawn.to_s} on #{transaction_time}[/]\n") 
 	end
 
 # status method prints out account name, account number, and balance
@@ -59,71 +68,59 @@ attr_accessor :name, :account_number, :balance, :password
 		puts Formatador.display_line("[green]------------------------------------------------[/]")
 	end
 
-# atm method allows customer to call transactions method
-	def atm(selection)
+# atm_transactions method allows customer to call deposit, withdrawal, and status methods
+	def atm_transactions
+		puts "What would you like to do today? "
+		puts Formatador.display("[green]Enter 1[/] to see account balance")
+		puts Formatador.display("[green]Enter 2[/] to make a deposit")
+		puts Formatador.display("[green]Enter 3[/] to make a withdrawal")
+		puts Formatador.display("[green]Enter Q[/] to quit")
+		print "Selection: "
+		selection = gets.chomp
 		# 1 is account status
 		if selection == "1"
 			status
-			transactions
+			atm_transactions
 		# 2 is deposit	
 		elsif selection == "2"
 			deposit
-			transactions
+			atm_transactions
 		# 3 is withdrawal
 		elsif selection == "3"
 			withdrawal
-			transactions
-		# 4 else writes updated info on customer (i.e., change in balance due to deposits/withdrawals) to customer's yaml file
+			atm_transactions
+		# 4 else writes updated info on customer (i.e., change in balance due to deposits/withdrawals) to customer's yaml file and ATM closes
 		else 
-			customer_file = "#{@name}" + ".yaml"
-				File.open(customer_file, 'w') do |file|  
-  			file.puts YAML::dump(self)
-  			end
+			update_record(@name)
+			puts "Your session is over. Thanks for banking with us!\n"		
 		end			
 	end
 
-# transactions methods allows customer to check account status or make depsoits/withdrawals
-# Must enter password each time you want to make a transaction. Customer has 3 tries to enter correct password.
-	def transactions
+# check_password method confirms password before atm_transactions is called. Customer has 3 tries to enter correct password.
+	def check_password
 		password_attempts = 2
 		puts "Please enter your password: "
 		prompt
 		answer = gets.chomp
-		# calls check_password method. If check_password true (i.e. entered correct password), lists transaction options.
-		if check_password(answer) == true
-			puts "What would you like to do today? "
-			puts Formatador.display("[green]Enter 1[/] to see account balance")
-			puts Formatador.display("[green]Enter 2[/] to make a deposit")
-			puts Formatador.display("[green]Enter 3[/] to make a withdrawal")
-			puts Formatador.display("[green]Enter Q[/] to quit")
-			print "Selection: "
-			selection = gets.chomp
-			# calls atm method bassed on what number they have selected
-			atm(selection)
+		#If answer matches password, calls atm_transactions method and customer can proceed.
+		if answer == @password
+			atm_transactions
 		else
 			until password_attempts == 0
 				puts "Re-enter your password. You have #{password_attempts} more tries."
 				prompt
 				answer = gets.chomp
-				check_password(answer)
 				password_attempts -=1
 			end
-			puts "You have no more password attempts. Please try again later."
+			puts "You have no more password attempts. Please try again later.\n"
 		end
-		# writes updated info on customer (i.e., change in balance due to deposits/withdrawals) to customer's yaml file
-		customer_file = "#{@name}" + ".yaml"
-		File.open(customer_file, 'w') do |file|  
-  			file.puts YAML::dump(self)
-  		end
 	end
 
-# check_password methods confirms the customer has entered the correct password
-	def check_password(entered_password)
-		if entered_password == @password
-			true
-		else 
-			puts "You entered the wrong password."
-		end
+	# welcome greets customer, calls check_password method
+	def welcome
+		puts "Welcome to the ATM at Bank of Ruby, #{@name}!"
+		check_password
 	end
+
 end
 end
